1e8949c8dc09bb5636d96fc977e7ccf2c4457d20
asherp <apembroke@gmail.com>
2018-05-22 15:46:08 -0400

    basic setup.py functionality

--- setup.py
+++ setup.py
@@ -1,2 +1,10 @@
+from distutils.core import setup
 
+setup(
+    name='pyTIEGCM',
+    version='0.1dev',
+    packages=['tiegcm',],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
 
